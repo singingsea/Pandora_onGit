d2f7cd835ae9c579ababaa8e86a7dad6c8bd44ed
Xiaoyi <33134727+singingsea@users.noreply.github.com>
2017-11-07 14:58:20 -0500

    add lists needed by correction code

---
+++ BlikP/rcodes_list.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 25 14:02:25 2017
+
+@author: ZhaoX
+"""
+
+# rcode in this list can be ploted by "plot_BlickP_L2", if a new rcode is used in retrieval, please add its name here and also relative information (eg. target trace gas column number) to "plot_BlickP_L2"
+retrieval_rcodes = ['rout0' , 'rnvs0', 'rfus0', 'rsut1', 'rsut2']
---
+++ BlikP/sites_list.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 25 14:03:15 2017
+
+@author: ZhaoX
+"""
+
+# [add site name to this list, otherwise the code will try to interprete the site name from file name (may give wrong site name!)
+sites_list = {'Downsview': 'America/Toronto', 'FortMcKay': 'America/Edmonton', 'StGeorge':'America/Toronto'}
