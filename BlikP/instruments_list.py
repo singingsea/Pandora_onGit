03ca7f34e44a1010bae9a882084679b558b96bb7
Xiaoyi <33134727+singingsea@users.noreply.github.com>
2017-12-13 11:53:29 -0500

    add instruments list
    this list now is used for auto weekly processing

---
+++ BlikP/instruments_list.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 13 11:35:16 2017
+
+@author: ZhaoX
+"""
+
+# [add instrument name to this dictionary "instrument name": "serial number")
+instruments_list = {'Pandora103': '103', 'Pandora104': '104', 
+              'Pandora108': '108', 'Pandora109': '109', 
+              'Pandora122': '122', 'Pandora123': '123', 
+              }
