0dbed2e37a539756eb2a2dd9343370582c696776
Xiaoyi <33134727+singingsea@users.noreply.github.com>
2017-12-13 12:19:12 -0500

    remove reset

--- BlikP/BlickP_L2_batch_run.py
+++ BlikP/BlickP_L2_batch_run.py
@@ -11,7 +11,7 @@ Created on Thu Sep 28 09:33:10 2017
 
 @author: ZhaoX
 """
-f#rom IPython import get_ipython ## house keeping, first two lines to clear workspace
+#from IPython import get_ipython ## house keeping, first two lines to clear workspace
 #get_ipython().magic('reset -sf') 
 
 import pandas as pd
