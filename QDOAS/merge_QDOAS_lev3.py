928d9175d0443d83071b0deab31e74c49633e6e0
singingsea <33134727+singingsea@users.noreply.github.com>
2018-03-12 23:56:53 -0400

    updates
    still need more twig, just upload to work on ec laptop

--- QDOAS/merge_QDOAS_lev3.py
+++ QDOAS/merge_QDOAS_lev3.py
@@ -58,33 +58,48 @@ df_QDOAS.sort_values(by = 'UTC', inplace = True)
 df_lev3.sort_values(by = 'UTC', inplace = True)
 merged_data = pd.merge_asof(df_QDOAS, df_lev3, on='UTC',tolerance=pd.Timedelta('1hr'))
 
-merged_data.y = merged_data['NO2_Vis.SlCol(no2)']
+merged_data = merged_data.dropna(subset=['NO2_Vis.SlCol(no2)']).dropna(subset=['NO2_VCD']).dropna(subset=['Fluxes 450']).dropna(subset=['Fluxes 500'])
+merged_data = merged_data[merged_data['NO2_Vis.SlCol(no2)']<0.5e37]
+merged_data = merged_data[merged_data['Column 31: Uncertainty of nitrogen dioxide slant column amount [Dobson Units], negative value=not fitted or fitting not successfull']<5][merged_data['Column 31: Uncertainty of nitrogen dioxide slant column amount [Dobson Units], negative value=not fitted or fitting not successfull']>0]
+merged_data = merged_data[merged_data['Column 21: Fitting result index: 1,2=no error, >2=error']<=2]
+merged_data = merged_data[merged_data['Column 64: Integration time [ms]']<=500]
+merged_data = merged_data[merged_data['NO2_Vis.SlErr(no2)']<=1e17]
+DU = 2.6870e+16
+merged_data.y = merged_data['NO2_Vis.SlCol(no2)']/DU
 merged_data.x = merged_data['NO2_VCD']
 merged_data.ci = merged_data['Fluxes 450']/merged_data['Fluxes 500']
 
 
 font = {'family' : 'DejaVu Sans', 'weight' : 'bold', 'size'   : 12}
 plt.rc('font', **font)
-fig = plt.figure(figsize=(22, 18), dpi=200, facecolor='w', edgecolor='k') 
+fig = plt.figure(figsize=(10, 10), dpi=200, facecolor='w', edgecolor='k') 
 cm = plt.cm.get_cmap('jet')
-gs = gridspec.GridSpec(1, 1, height_ratios=[1], width_ratios=[1]) 
+gs = gridspec.GridSpec(1, 2, height_ratios=[1], width_ratios=[1,1]) 
 
 ax0 = plt.subplot(gs[0])
 #sc = ax0.scatter(merged_data.x, merged_data.y, merged_data.ci, s=10, cmap=cm)
-sc = ax0.scatter(merged_data.x, merged_data.y, merged_data.ci, cmap=cm)
+sc = ax0.scatter(merged_data.x, merged_data.y, c = merged_data.ci, s = 10, cmap=cm)
 plt.colorbar(sc).set_label('CI')
 ax0.grid()
-ax0.set_ylabel(ylabel_1 + ' dSCD [DU]')
-ax0.set_xlabel('VCD [DU]')
+ax0.set_ylabel('ZS dSCD [DU]')
+ax0.set_xlabel('DS VCD [DU]')
 ax0.legend()
-ax0.set_title(filepath)
+ax0.set_title('ZS vs. DS NO2')
+
+
+ax1 = plt.subplot(gs[1])
+sc = ax1.scatter(merged_data.SZA, merged_data.y/merged_data.x, c = merged_data.ci, s= 10, cmap=cm)
+plt.colorbar(sc).set_label('CI')
+ax1.grid()
+ax1.set_ylabel('ZS AMF')
+ax1.set_xlabel('SZA')
+ax1.legend()
+ax1.set_title('ZS vs. DS NO2')
 
 plt.tight_layout()
 plt.savefig(plotpath +'test.png')
 #plt.show()
 plt.close()
-
-
 
 
 
