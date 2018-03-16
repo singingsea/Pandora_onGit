fbc5233d0cdc81b46184811afd15a8c24851a1a7
Xiaoyi <33134727+singingsea@users.noreply.github.com>
2018-03-16 16:00:09 -0400

    estimate RCD

--- QDOAS/estimate_RCD.m
+++ QDOAS/estimate_RCD.m
@@ -1,10 +1,14 @@
 function [p,stats] = estimate_RCD(data)
 addpath('C:\Users\ZhaoX\Documents\MATLAB\matlab\quantreg');
+% AMF filter
+%TF = data.ndacc_amf <= 5;
+TF = (data.ndacc_amf >= 2) & (data.ndacc_amf <= 10);
+data = data(TF,:);
 
 x = data.ndacc_amf;
 y = data.dSCD;
 
-[p,stats]=quantreg(x,y,.1,1);
+[p,stats]=quantreg(x,y,0.05,1);
 figure;hold all;
 plot(x,y,'b.',x,polyval(p,x),x,stats.yfitci,'k:');
 legend('data','1st order 10th percentile fit','95% confidence interval','location','best'); 
