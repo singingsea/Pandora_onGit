ee54d59d18f4c056ffef213fa549249be2845523
Xiaoyi <33134727+singingsea@users.noreply.github.com>
2018-03-28 14:14:35 -0400

    add new site location to list

--- BlikP/sites_list.py
+++ BlikP/sites_list.py
@@ -6,4 +6,4 @@ Created on Wed Oct 25 14:03:15 2017
 """
 
 # [add site name to this list, otherwise the code will try to interprete the site name from file name (may give wrong site name!)
-sites_list = {'Downsview': 'America/Toronto', 'FortMcKay': 'America/Edmonton', 'StGeorge':'America/Toronto'}
+sites_list = {'Downsview': 'America/Toronto', 'FortMcKay': 'America/Edmonton', 'StGeorge':'America/Toronto','Egbert':'America/Toronto'}
