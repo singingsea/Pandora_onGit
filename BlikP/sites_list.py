ba14601987e92e0ffda7c6fde9bcc69a66273642
Zhao <Xiaoyi.Zhao@ec.gc.ca>
2019-05-18 19:54:54 -0400

    include new site
    include new site PEARL EUREKA!

--- BlikP/sites_list.py
+++ BlikP/sites_list.py
@@ -7,5 +7,5 @@ Created on Wed Oct 25 14:03:15 2017
 
 # [add site name to this list, otherwise the code will try to interprete the site name from file name (may give wrong site name!)
 #sites_list = {'Downsview': 'America/Toronto', 'FortMcKay': 'America/Edmonton', 'StGeorge':'America/Toronto','Egbert':'America/Toronto'}
-sites_list = {'Downsview': 'America/Toronto', 'FortMcKay': 'America/Edmonton', 'OilSands': 'America/Edmonton','StGeorge':'America/Toronto','Egbert':'America/Toronto'}
+sites_list = {'Downsview': 'America/Toronto', 'FortMcKay': 'America/Edmonton', 'OilSands': 'America/Edmonton','StGeorge':'America/Toronto','Egbert':'America/Toronto','PEARL':'Etc/GMT-5'}
 
